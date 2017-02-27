cfc9cfcb3e07a96112df10e4363d42c98f7d7615
datalal <lawsonw2@gmail.com>
2017-02-27 09:16:09 -0500

    b/w theme started

--- js/reddit.js
+++ js/reddit.js
@@ -4,20 +4,16 @@ app.controller("redditCtrl", function($scope,$http) {
   $http.get("https://www.reddit.com/r/awww.json")
   .then(function(response) {
 
-for (i = 0; i < 10; i++){
-    $scope.content = response.data.data;
-    $scope.title = response.data.data.children[0].data.title;
-    $scope.domain = response.data.data.children[0].data.domain;
-    $scope.comments = response.data.data.children[0].data.num_comments;
-    $scope.author = response.data.data.children[0].data.author;
-    $scope.thumbnail = response.data.data.children[0].data.thumbnail;
-    $scope.permalink = response.data.data.children[0].data.permalink;
+
     $scope.arrayTest = response.data.data.children;
 
-}
 
-// var myDate = new Date(1000*$blogDate);
-
+// function calcHoursPosted(i){
+// var myDate = new Date(1000*i);
+// $scope.hoursPosted = myDate.getHours();
+// return i;
+// console.log("cal chours");
+//}
 
   }, function(response) {
 
